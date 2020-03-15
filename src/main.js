6d3d6ac6634bd8f431a0756c92843e3386dba575
cc032191 <373841272@qq.com>
2020-03-15 15:33:40 +0800

    搭建项目结构完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from './router/index'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+
+// 防止同路由点击报错
+// const routerPush = VueRouter.prototype.push
+// VueRouter.prototype.push = function push(location) {
+//   return routerPush.call(this, location).catch(error => error)
+// }
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
