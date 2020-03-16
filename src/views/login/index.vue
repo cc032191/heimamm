5b8ba35b6d29373acc2c69ba4f2bd4f87a8cb35f
cc032191 <373841272@qq.com>
2020-03-16 16:09:46 +0800

    完成登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,14 +7,94 @@
         <span class="title-long"></span>
         <span class="title-login">用户登录</span>
       </div>
-      
+      <!-- 表单区域 -->
+      <el-form ref="form" :rules="rules" :model="form" label-width="0px" class="login-form">
+        <el-form-item prop="phonecode">
+          <el-input prefix-icon="el-icon-user" placeholder="请输入手机号" v-model="form.phonecode"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input prefix-icon="el-icon-lock" placeholder="请输入密码" v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item prop="login">
+          <el-row>
+            <el-col :span="18">
+              <el-input prefix-icon="el-icon-key" placeholder="请输入验证码" v-model="form.login"></el-input>
+            </el-col>
+            <el-col :span="6">
+              <img src="../../assets/login_captcha.png" class="login-img" alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item prop="type">
+          <el-checkbox v-model="form.type">
+            我已阅读并同意
+            <el-link type="primary">用户协议</el-link>和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btnform" type="primary" @click="register()">登录</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btnform" type="primary">注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <img src="../../assets/login_banner_ele.png" alt />
+    <register></register>
   </div>
 </template>
 
 <script>
-export default {};
+import register from "./components/register";
+export default {
+  data() {
+    return {
+      form: {
+        phonecode: "",
+        password: "",
+        login: "",
+        type: []
+      },
+      rules: {
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 5, max: 10, message: "长度在 5 到 10 个字符", trigger: "blur" }
+        ],
+        login: [
+          { required: true, message: "请输入验证码", trigger: "blur" },
+          { min: 4, max: 4, message: "长度为 4 个字符", trigger: "blur" }
+        ],
+        type: [
+          {
+            type: "array",
+            required: true,
+            message: "请确认已阅读",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  components: {
+    register
+  },
+  methods: {
+    register() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.$message({
+            message: "恭喜你，这是一条成功消息",
+            type: "success"
+          });
+        } else {
+          this.$message.error("错了哦，这是一条错误消息");
+          return false;
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style lang='less'>
@@ -61,6 +141,19 @@ export default {};
         color: rgba(12, 12, 12, 1);
         margin-left: 12px;
       }
+    }
+    .login-form {
+      margin-top: 29px;
+    }
+    .btnform {
+      width: 100%;
+    }
+    .el-form-item__content {
+      line-height: 25px;
+    }
+    .login-img {
+      height: 42px;
+      width: 100%;
     }
   }
   img {
